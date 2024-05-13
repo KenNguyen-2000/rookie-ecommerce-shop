f711cc2dec528fd417bced43fc579ca075271542
Ken Nguyen <ngkien299@gmail.com>
2024-05-13 18:16:27 +0700

    Initialize entities

--- src/RookEcomShop.Domain/Entities/Order.cs
+++ src/RookEcomShop.Domain/Entities/Order.cs
@@ -1,11 +1,6 @@
 ﻿using RookEcomShop.Domain.Common;
 using RookEcomShop.Domain.Common.Enums;
-using System;
-using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations.Schema;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace RookEcomShop.Domain.Entities
 {
