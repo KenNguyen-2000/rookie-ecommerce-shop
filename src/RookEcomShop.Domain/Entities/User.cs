74fe1e0487a42c0f830c745f777fcc5b798058e5
Ken Nguyen <ngkien299@gmail.com>
2024-05-14 18:07:09 +0700

    update: Update models mapping IdentityUser

--- src/RookEcomShop.Domain/Entities/User.cs
+++ src/RookEcomShop.Domain/Entities/User.cs
@@ -1,20 +1,18 @@
-﻿using RookEcomShop.Domain.Common;
+﻿using Microsoft.AspNetCore.Identity;
+using RookEcomShop.Domain.Common;
 using RookEcomShop.Domain.Common.Enums;
 
 namespace RookEcomShop.Domain.Entities
 {
-    public class User : BaseEntity
+    public class User : IdentityUser<int>
     {
         public string FirstName { get; set; } = null!;
         public string LastName { get; set; } = null!;
-        public string Email { get; set; } = null!;
-        public string? HashedPassword { get; set; }
-        public string? PhoneNumber { get; set; }
+        public string? Address { get; set; }
         public DateTime? Dob { get; set; }
         public Gender Gender { get; set; } = Gender.Unisex;
         public string? AvartarUrl { get; set; }
 
-        public virtual ICollection<UserRole> Role { get; set; } = new HashSet<UserRole>();
         public virtual Cart? Cart { get; set; }
         public virtual ICollection<Review> Reviews { get; set; } = new List<Review>();
         public virtual ICollection<Order> Orders { get; set; } = new List<Order>();
