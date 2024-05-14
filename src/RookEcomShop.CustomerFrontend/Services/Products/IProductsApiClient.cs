cb8466bd42175c129056fe671c1dbe3cc305b31c
Ken Nguyen <ngkien299@gmail.com>
2024-05-14 22:23:59 +0700

    Add product api client connect api

---
+++ src/RookEcomShop.CustomerFrontend/Services/Products/IProductsApiClient.cs
@@ -0,0 +1,9 @@
+﻿using RookEcomShop.ViewModels.Product;
+
+namespace RookEcomShop.CustomerFrontend.Services.Products
+{
+    public interface IProductsHttpClient
+    {
+        Task<IEnumerable<ProductVM>> GetProductsAsync();
+    }
+}
---
+++ src/RookEcomShop.CustomerFrontend/Services/Products/ProductsApiClient.cs
@@ -0,0 +1,29 @@
+﻿using RookEcomShop.ViewModels.Product;
+using Microsoft.AspNetCore.Authentication;
+using System.Text.Json;
+
+namespace RookEcomShop.CustomerFrontend.Services.Products
+{
+    public class ProductsApiClient : IProductsHttpClient
+    {
+        private readonly HttpClient _httpClient;
+
+        public ProductsApiClient(HttpClient httpClient, IHttpContextAccessor httpContext)
+        {
+            _httpClient = httpClient;
+            _httpClient.BaseAddress = new Uri("https://localhost:5003/");
+        }
+
+        public async Task<IEnumerable<ProductVM>> GetProductsAsync()
+        {
+            var response = await _httpClient.GetAsync("api/v1/products");
+
+            response.EnsureSuccessStatusCode();
+
+            string content = await response.Content.ReadAsStringAsync();
+            var productList = JsonSerializer.Deserialize<IList<ProductVM>>(content)!;
+
+            return productList;
+        }
+    }
+}
