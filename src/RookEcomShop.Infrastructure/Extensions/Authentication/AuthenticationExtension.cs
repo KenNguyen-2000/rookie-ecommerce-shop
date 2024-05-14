adce4656c7920e3f0478b23902c4bd9302e49264
Ken Nguyen <ngkien299@gmail.com>
2024-05-14 13:40:41 +0700

    Add Microsoft.Authentication.JwtBearer package and Confgiure JwtBearer options

---
+++ src/RookEcomShop.Infrastructure/Extensions/Authentication/AuthenticationExtension.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RookEcomShop.Infrastructure.Extensions.Authentication
+{
+    public static class AuthenticationExtension
+    {
+        public static void ConfigureAuthentication(this IServiceCollection services, IConfiguration configuration)
+        {
+            var JwtSettings = new JwtSettings();
+            configuration.Bind(JwtSettings.SectionName, JwtSettings);
+
+            services
+                .AddAuthentication(defaultScheme: JwtBearerDefaults.AuthenticationScheme)
+                .AddJwtBearer("Bearer", jwtBearerOptions =>
+                {
+                    jwtBearerOptions.TokenValidationParameters = new TokenValidationParameters
+                    {
+                        ValidateIssuerSigningKey = true,
+                        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(JwtSettings.Secret)),
+                        ValidateIssuer = true,
+                        ValidIssuer = configuration["JwtSettings:Issuer"],
+                        ValidateAudience = true,
+                        ValidAudience = configuration["JwtSettings:Audience"],
+                        ValidateLifetime = true,
+                        ClockSkew = TimeSpan.Zero
+                    };
+                });
+        }
+    }
+}
---
+++ src/RookEcomShop.Infrastructure/Extensions/Authentication/JwtSettings.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RookEcomShop.Infrastructure.Extensions.Authentication
+{
+    public class JwtSettings
+    {
+        public const string SectionName = "JwtSettings";
+        public string Secret { get; set; } = null!;
+        public int ExpiryMinutes { get; set; }
+        public string Issuer { get; set; } = null!;
+        public string Audience { get; set; } = null!;
+    }
+}
