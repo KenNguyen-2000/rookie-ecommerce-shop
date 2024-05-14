91f8738c3582c6f061fa57ccb1fa4afc731f70c3
Ken Nguyen <ngkien299@gmail.com>
2024-05-14 22:23:39 +0700

    Configure Opendic connection

--- src/RookEcomShop.CustomerFrontend/Program.cs
+++ src/RookEcomShop.CustomerFrontend/Program.cs
@@ -1,9 +1,31 @@
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultScheme = "Cookies";
+    options.DefaultChallengeScheme = "oidc";
+})
+    .AddCookie("Cookies")
+    .AddOpenIdConnect("oidc", options =>
+    {
+        options.Authority = "https://localhost:5001";
+
+        options.ClientId = "rookEcomShop.mvc";
+        options.ClientSecret = "49C1A7E1-0C79-4A89-A3D6-A37998FB86B0";
+        options.ResponseType = "code";
+
+        options.SaveTokens = true;
+
+        options.Scope.Add("rookEcomShop.api");
+        options.Scope.Add("offline_access");
+    });
+
 var app = builder.Build();
+
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
