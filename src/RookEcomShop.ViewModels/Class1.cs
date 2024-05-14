a04b85d48d54599223bdf0812b8e4b59bc782eb4
Ken Nguyen <ngkien299@gmail.com>
2024-05-14 18:08:33 +0700

    Add identity request dto and product view models

--- src/RookEcomShop.ViewModels/Class1.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace RookEcomShop.ViewModels;
-
-public class Class1
-{
-
-}
---
+++ src/RookEcomShop.ViewModels/Identity/RegisterRequest.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RookEcomShop.ViewModels.Identity
+{
+    public class RegisterRequest
+    {
+
+        public string Email { get; set; } = null!;
+        public string FirstName { get; set; } = null!;
+        public string LastName { get; set; } = null!;
+        public string Password { get; set; } = null!;
+        public string ConfirmPassword { get; set; } = null!;
+        public string? PhoneNumber { get; set; }
+        public DateTime Dob { get; set; }
+    }
+}
