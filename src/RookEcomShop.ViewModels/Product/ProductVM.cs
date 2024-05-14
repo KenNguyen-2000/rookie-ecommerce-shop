5c6a8cc2a1b338d41f164c48d70cf6ca5c71e092
Ken Nguyen <ngkien299@gmail.com>
2024-05-14 22:24:11 +0700

    Add product view model

---
+++ src/RookEcomShop.ViewModels/Product/ProductVM.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RookEcomShop.ViewModels.Product
+{
+    public class ProductVM
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = null!;
+        public string Description { get; set; } = null!;
+        public decimal Price { get; set; }
+    }
+}
