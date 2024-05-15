cabd64b553021e82caebe2c21b1295cdad07a3e9
Ken Nguyen <ngkien299@gmail.com>
2024-05-15 18:35:15 +0700

    Initialize custom Response object

---
+++ src/RookEcomShop.ViewModels/Api/Response.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RookEcomShop.ViewModels.Api
+{
+    public class Response<T>
+    {
+        public bool Succeeded { get; set; } = false;
+        public string Message { get; set; } = null!;
+        public T? Data { get; set; }
+        public IEnumerable<T>? Datas { get; set; }
+        public int Count { get; set; } = 0;
+        public HttpStatusCode Status { get; set; } = HttpStatusCode.InternalServerError;
+
+        public Response(string message, bool success = false, HttpStatusCode status = default)
+        {
+            Succeeded = success;
+            Message = message;
+            Status = status;
+        }
+        public Response(IEnumerable<T> datas, string message, int count = 0, HttpStatusCode status = HttpStatusCode.OK)
+        {
+            Succeeded = true;
+            Message = message;
+            Datas = datas;
+            Count = count != 0 ? count : Datas.Count();
+            Status = status;
+        }
+        public Response(T data, string message, HttpStatusCode status = HttpStatusCode.OK)
+        {
+            Succeeded = true;
+            Message = message;
+            Data = data;
+            Status = status;
+        }
+    }
+}
