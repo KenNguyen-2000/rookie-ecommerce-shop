39434f5050a8652cd2db79c27472383749eac9aa
Ken Nguyen <ngkien299@gmail.com>
2024-05-15 14:19:51 +0700

    Inherit BaseRepository

---
+++ src/RookEcomShop.Application/Common/Repositories/IUserRepository.cs
@@ -0,0 +1,10 @@
+﻿using RookEcomShop.Domain.Entities;
+
+namespace RookEcomShop.Application.Common.Repositories
+{
+    public interface IUserRepository
+    {
+        Task<User?> GetUserByEmailAsync(string email);
+        Task<User?> GetUserByIdAsync(int id);
+    }
+}
---
+++ src/RookEcomShop.Infrastructure/Persistence/Repositories/UserRepository.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RookEcomShop.Application.Common.Repositories;
+using RookEcomShop.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static IdentityServer4.Models.IdentityResources;
+
+namespace RookEcomShop.Infrastructure.Persistence.Repositories
+{
+    public class UserRepository : IUserRepository
+    {
+        private readonly RookEcomShopDbContext _context;
+
+        public UserRepository(RookEcomShopDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<User?> GetUserByEmailAsync(string email)
+        {
+            return await _context.Users.FirstOrDefaultAsync(x => x.Email == email);
+        }
+
+        public async Task<User?> GetUserByIdAsync(int id)
+        {
+            return await _context.Users.FirstOrDefaultAsync(x => x.Id == id);
+        }
+    }
+}
