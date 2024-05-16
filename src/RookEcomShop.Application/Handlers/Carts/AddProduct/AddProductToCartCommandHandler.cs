936d35ddda76b6e68eedab693f814225cbc5c6a3
Ken Nguyen <ngkien299@gmail.com>
2024-05-16 17:27:33 +0700

    update: Optimize by seperating funtion into CreateCartIfExists and AddProductToCart

--- src/RookEcomShop.Application/Handlers/Carts/AddProduct/AddProductToCartCommandHandler.cs
+++ src/RookEcomShop.Application/Handlers/Carts/AddProduct/AddProductToCartCommandHandler.cs
@@ -4,6 +4,7 @@ using RookEcomShop.Application.Common.Exceptions;
 using RookEcomShop.Application.Common.Repositories;
 using RookEcomShop.Application.Handlers.Carts.Create;
 using RookEcomShop.Domain.Entities;
+using static Microsoft.EntityFrameworkCore.DbLoggerCategory.Database;
 
 namespace RookEcomShop.Application.Handlers.Carts.AddProduct
 {
@@ -30,23 +31,43 @@ namespace RookEcomShop.Application.Handlers.Carts.AddProduct
         {
             var product = await _productRepository.GetByIdAsync(command.ProductId);
             if (product == null)
+            {
                 throw new NotFoundException($"Product with id {command.ProductId} not found!");
+            }
 
-            var userContext = 1;
+            var userContext = 2; // Assuming userContext is retrieved elsewhere
 
             var cart = await _cartRepository.GetCartByUserIdAsync(userContext);
-            if (cart is null)
-                await _sender.Send(new CreateCartCommand { UserId = userContext });
+            cart = cart ?? await CreateCartIfNotExists(userContext, cancellationToken);
 
-            cart.CartDetails.Add(new CartDetail
-            {
-                Product = product,
-                Quantity = command.Quantity
-            });
+            AddProductToCart(cart, product, command.Quantity);
 
             await _unitOfWork.SaveAsync(cancellationToken);
 
             return Result.Ok();
+        }
+
+        private async Task<Cart> CreateCartIfNotExists(int userId, CancellationToken cancellationToken)
+        {
+            await _sender.Send(new CreateCartCommand { UserId = userId }, cancellationToken);
+            return (await _cartRepository.GetCartByUserIdAsync(userId))!;
+        }
+
+        private static void AddProductToCart(Cart cart, Product product, int quantity)
+        {
+            var cartDetail = cart.CartDetails.FirstOrDefault(cd => cd.Product.Id == product.Id);
+            if (cartDetail != null)
+            {
+                cartDetail.Quantity += quantity;
+            }
+            else
+            {
+                cart.CartDetails.Add(new CartDetail
+                {
+                    Product = product,
+                    Quantity = quantity
+                });
+            }
         }
     }
 }
