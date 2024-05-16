819d0127e007a85d6d4ca4c65308dd0ac1c5cec1
Ken Nguyen <ngkien299@gmail.com>
2024-05-16 15:07:45 +0700

    feat(api): Delete product by id

---
+++ src/RookEcomShop.Application/Handlers/Products/DeleteById/DeleteProductByIdCommand.cs
@@ -0,0 +1,9 @@
+﻿using MediatR;
+
+namespace RookEcomShop.Application.Handlers.Products.DeleteById
+{
+    public class DeleteProductByIdCommand : IRequest
+    {
+        public int Id { get; set; }
+    }
+}
---
+++ src/RookEcomShop.Application/Handlers/Products/DeleteById/DeleteProductByIdCommandHandler.cs
@@ -0,0 +1,28 @@
+﻿using MediatR;
+using RookEcomShop.Application.Common.Exceptions;
+using RookEcomShop.Application.Common.Repositories;
+
+namespace RookEcomShop.Application.Handlers.Products.DeleteById
+{
+    public class DeleteProductByIdCommandHandler : IRequestHandler<DeleteProductByIdCommand>
+    {
+        private readonly IProductRepository _productRepository;
+        private readonly IUnitOfWork _unitOfWork;
+
+        public DeleteProductByIdCommandHandler(IProductRepository productRepository, IUnitOfWork unitOfWork)
+        {
+            _productRepository = productRepository;
+            _unitOfWork = unitOfWork;
+        }
+
+        public async Task Handle(DeleteProductByIdCommand command, CancellationToken cancellationToken)
+        {
+            var product = await _productRepository.GetByIdAsync(command.Id);
+            if (product == null)
+                throw new NotFoundException($"Product with id {command.Id} not found");
+
+            _productRepository.Delete(product);
+            await _unitOfWork.SaveAsync();
+        }
+    }
+}
