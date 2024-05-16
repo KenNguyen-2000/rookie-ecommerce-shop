99ab3a4435b3df947e5b96d05128dfa8e69c148a
Ken Nguyen <ngkien299@gmail.com>
2024-05-16 17:30:47 +0700

    Registering order repository service

---
+++ src/RookEcomShop.Application/Common/Repositories/IOrderRepository.cs
@@ -0,0 +1,13 @@
+﻿using RookEcomShop.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RookEcomShop.Application.Common.Repositories
+{
+    public interface IOrderRepository : IBaseRepository<Order>
+    {
+    }
+}
---
+++ src/RookEcomShop.Infrastructure/Persistence/Repositories/OrderRepository.cs
@@ -0,0 +1,17 @@
+﻿using RookEcomShop.Application.Common.Repositories;
+using RookEcomShop.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RookEcomShop.Infrastructure.Persistence.Repositories
+{
+    public class OrderRepository : BaseRepository<Order>, IOrderRepository
+    {
+        public OrderRepository(RookEcomShopDbContext context) : base(context)
+        {
+        }
+    }
+}
