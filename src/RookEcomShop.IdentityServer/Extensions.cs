622734306037e4debf845f74c119e178be665ef6
Ken Nguyen <ngkien299@gmail.com>
2024-05-18 17:52:33 +0700

    feat: Add LoadingPage extension method for redirecting with a loading page

--- src/RookEcomShop.IdentityServer/Extensions.cs
+++ src/RookEcomShop.IdentityServer/Extensions.cs
@@ -1,5 +1,6 @@
 ﻿using IdentityServer4.Models;
 using Microsoft.AspNetCore.Mvc;
+using RookEcomShop.IdentityServer.Models;
 
 namespace RookEcomShop.IdentityServer
 {
@@ -14,6 +15,12 @@ namespace RookEcomShop.IdentityServer
             return !context.RedirectUri.StartsWith("https", StringComparison.Ordinal)
                && !context.RedirectUri.StartsWith("http", StringComparison.Ordinal);
         }
+        public static IActionResult LoadingPage(this Controller controller, string viewName, string redirectUri)
+        {
+            controller.HttpContext.Response.StatusCode = 200;
+            controller.HttpContext.Response.Headers["Location"] = "";
 
+            return controller.View(viewName, new RedirectViewModel { RedirectUrl = redirectUri });
+        }
     }
 }
---
+++ src/RookEcomShop.IdentityServer/Models/RedirectViewModel.cs
@@ -0,0 +1,7 @@
+﻿namespace RookEcomShop.IdentityServer.Models
+{
+    public class RedirectViewModel
+    {
+        public string RedirectUrl { get; set; }
+    }
+}
