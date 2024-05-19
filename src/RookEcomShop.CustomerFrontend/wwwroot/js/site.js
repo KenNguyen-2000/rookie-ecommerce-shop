78248ecde55c4cf96af3e24be5073d7be4402a75
Ken Nguyen <ngkien299@gmail.com>
2024-05-19 11:14:20 +0700

    feat: Add user menu toggle functionality

--- src/RookEcomShop.CustomerFrontend/wwwroot/js/site.js
+++ src/RookEcomShop.CustomerFrontend/wwwroot/js/site.js
@@ -2,3 +2,26 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+// <reference path='~/Scripts/jquery-3.7.1.js' />;
+$('#user-menu-button').on('click', () => {
+  var userMenu = $('[aria-labelledby="user-menu-button"]');
+  if (userMenu.hasClass('invisible')) {
+    userMenu.removeClass(
+      'transition ease-out duration-100 transform opacity-0 scale-95 invisible'
+    );
+    userMenu.addClass(
+      'transition ease-in duration-75 transform opacity-100 scale-100'
+    );
+  } else {
+    userMenu.removeClass(
+      'transition ease-in duration-75 transform opacity-100 scale-100'
+    );
+    userMenu.addClass(
+      'transition ease-out duration-100 transform opacity-0 scale-95'
+    );
+    setTimeout(() => {
+      userMenu.addClass('invisible');
+    }, 100);
+  }
+});
