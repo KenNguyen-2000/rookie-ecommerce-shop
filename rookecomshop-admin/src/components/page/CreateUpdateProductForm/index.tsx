108f1f53d0b7aad9116cfb4d1163eb31108cd429
Ken Nguyen <ngkien299@gmail.com>
2024-05-24 20:35:47 +0700

    refactor: CreatUpdateProductForm compoennt for create and update product page

---
+++ rookecomshop-admin/src/components/page/CreateUpdateProductForm/index.tsx
@@ -0,0 +1,311 @@
+import { ChevronLeft, Upload } from 'lucide-react';
+
+import { Badge } from '@/components/ui/badge';
+import { Button } from '@/components/ui/button';
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/components/ui/card';
+import { Input } from '@/components/ui/input';
+import { Label } from '@/components/ui/label';
+import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from '@/components/ui/select';
+import { Textarea } from '@/components/ui/textarea';
+import { SubmitHandler, useForm } from 'react-hook-form';
+import { CreateUpdateProductInputs } from '@/services/products/products.type';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { CreateUpdateProductScheme } from '@/services/products/products.scheme';
+import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from '@/components/ui/form';
+import { FC, useEffect } from 'react';
+import { useAppDispatch, useAppSelector } from '@/redux/reduxHooks';
+import { getCategoriesAsync } from '@/redux/thunks/categories.thunk';
+import { setSelectedTopCategory } from '@/redux/slices/categories.slice';
+
+type CreateUpdateProductFormProps ={
+    defaultValue?: CreateUpdateProductInputs;
+    handleSubmitForm: (args: CreateUpdateProductInputs) => void;
+}
+
+const CreateUpdateProductForm : FC<CreateUpdateProductFormProps> = ({defaultValue, handleSubmitForm}) => {
+    const dispatch = useAppDispatch();
+	const { categories, isLoading: categoryLoading, selectedTopCategory } = useAppSelector((state) => state.categories);
+	const form = useForm<CreateUpdateProductInputs>({
+		resolver: zodResolver(CreateUpdateProductScheme),
+		defaultValues: defaultValue,
+	});
+
+	const onSubmit: SubmitHandler<CreateUpdateProductInputs> = (data) => {
+        handleSubmitForm(data);
+    }
+
+    useEffect(() => {
+		dispatch(getCategoriesAsync());
+		if(defaultValue?.category)
+            {
+                
+            dispatch(setSelectedTopCategory(defaultValue.category));
+            }
+	}, [defaultValue?.categoryName]);
+  return (
+    <Form {...form}>
+				<form onSubmit={form.handleSubmit(onSubmit)}>
+					<div className="mx-auto grid max-w-[64rem] flex-1 auto-rows-max gap-4">
+						<div className="flex items-center gap-4">
+							<Button variant="outline" size="icon" className="h-7 w-7">
+								<ChevronLeft className="h-4 w-4" />
+								<span className="sr-only">Back</span>
+							</Button>
+							<h1 className="flex-1 shrink-0 whitespace-nowrap text-xl font-semibold tracking-tight sm:grow-0">
+								Pro Controller
+							</h1>
+							<Badge variant="outline" className="ml-auto sm:ml-0">
+								In stock
+							</Badge>
+							<div className="hidden items-center gap-2 md:ml-auto md:flex">
+								<Button variant="outline" size="sm">
+									Discard
+								</Button>
+								<Button size="sm">Save Product</Button>
+							</div>
+						</div>
+						<div className="grid gap-4 md:grid-cols-[1fr_250px] lg:grid-cols-3 lg:gap-8">
+							<div className="grid auto-rows-max items-start gap-4 lg:col-span-2 lg:gap-8">
+								<Card x-chunk="dashboard-07-chunk-0">
+									<CardHeader>
+										<CardTitle>Product Details</CardTitle>
+										<CardDescription>
+											Lipsum dolor sit amet, consectetur adipiscing elit
+										</CardDescription>
+									</CardHeader>
+									<CardContent>
+										<div className="grid gap-6">
+											<div className="grid gap-3">
+												<FormField
+													control={form.control}
+													name="name"
+													render={({ field }) => (
+														<FormItem>
+															<Label>Name</Label>
+															<FormControl>
+																<Input
+																	className="w-full"
+																	placeholder="Gamer Gear Pro Controller"
+																	{...field}
+																/>
+															</FormControl>
+															<FormMessage />
+														</FormItem>
+													)}
+												/>
+											</div>
+											<div className="grid grid-cols-3 gap-3">
+												<div className="grid">
+													<FormField
+														control={form.control}
+														name="price"
+														render={({ field }) => (
+															<FormItem>
+																<FormLabel>Price</FormLabel>
+																<FormControl>
+																	<Input
+																		type="number"
+																		className="w-full"
+																		placeholder="0"
+																		{...field}
+																	/>
+																</FormControl>
+																<FormMessage />
+															</FormItem>
+														)}
+													/>
+												</div>
+												<div className="grid">
+													<FormField
+														control={form.control}
+														name="stockQuantity"
+														render={({ field }) => (
+															<FormItem>
+																<FormLabel>Stock</FormLabel>
+																<FormControl>
+																	<Input
+																		type="number"
+																		className="w-full"
+																		placeholder="0"
+																		{...field}
+																	/>
+																</FormControl>
+																<FormMessage />
+															</FormItem>
+														)}
+													/>
+												</div>
+											</div>
+											<div className="grid gap-3">
+												<FormField
+													control={form.control}
+													name="description"
+													render={({ field }) => (
+														<FormItem>
+															<Label>Description</Label>
+															<FormControl>
+																<Textarea
+																	placeholder="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam auctor, nisl nec ultricies ultricies, nunc nisl ultricies nunc, nec ultricies nunc nisl nec nunc."
+																	className="min-h-32"
+																	{...field}
+																/>
+															</FormControl>
+															<FormMessage />
+														</FormItem>
+													)}
+												/>
+											</div>
+										</div>
+									</CardContent>
+								</Card>
+								<Card x-chunk="dashboard-07-chunk-2">
+									<CardHeader>
+										<CardTitle>Product Category</CardTitle>
+									</CardHeader>
+									<CardContent>
+										<div className="grid gap-6 sm:grid-cols-3">
+											<div className="grid gap-3">
+												<FormField
+													control={form.control}
+													name="categoryName"
+													render={({ field }) => (
+														<FormItem>
+															<FormLabel htmlFor="category">Category</FormLabel>
+															<Select defaultValue={defaultValue?.categoryName} onValueChange={(value) => {
+                                                                const getCategory = categories.find((category) => category.name === value);
+                                                                dispatch(setSelectedTopCategory(getCategory));
+                                                                field.onChange(value);
+                                                            }}>
+																<SelectTrigger
+																>
+																	<SelectValue placeholder="Select category" />
+																</SelectTrigger>
+																<SelectContent>
+																	{categories.map((category) => (
+																		<SelectItem
+																			key={category.name}
+																			value={category.name}
+																		>
+																			{category.name}
+																		</SelectItem>
+																	))}
+																</SelectContent>
+															</Select>
+														</FormItem>
+													)}
+												/>
+											</div>
+											<div className="grid gap-3">
+												<Label htmlFor="subcategory">Subcategory (optional)</Label>
+												<Select>
+													<SelectTrigger id="subcategory" aria-label="Select subcategory">
+														<SelectValue placeholder="Select subcategory" />
+													</SelectTrigger>
+													<SelectContent>
+														{categories.map((category) => (
+															<SelectItem key={category.name} value={category.name}>
+																{category.name}
+															</SelectItem>
+														))}
+													</SelectContent>
+												</Select>
+											</div>
+										</div>
+									</CardContent>
+								</Card>
+							</div>
+							<div className="grid auto-rows-max items-start gap-4 lg:gap-8">
+								<Card x-chunk="dashboard-07-chunk-3">
+									<CardHeader>
+										<CardTitle>Product Status</CardTitle>
+									</CardHeader>
+									<CardContent>
+										<div className="grid gap-6">
+											<div className="grid gap-3">
+												<Label htmlFor="status">Status</Label>
+												<Select>
+													<SelectTrigger id="status" aria-label="Select status">
+														<SelectValue placeholder="Select status" />
+													</SelectTrigger>
+													<SelectContent>
+														<SelectItem value="draft">Draft</SelectItem>
+														<SelectItem value="published">Active</SelectItem>
+														<SelectItem value="archived">Archived</SelectItem>
+													</SelectContent>
+												</Select>
+											</div>
+										</div>
+									</CardContent>
+								</Card>
+								<Card className="overflow-hidden" x-chunk="dashboard-07-chunk-4">
+									<CardHeader>
+										<CardTitle>Product Images</CardTitle>
+										<CardDescription>
+											Lipsum dolor sit amet, consectetur adipiscing elit
+										</CardDescription>
+									</CardHeader>
+									<CardContent>
+										<div className="grid gap-2">
+											<img
+												alt="Product image"
+												className="aspect-square w-full rounded-md object-cover"
+												height="300"
+												src="/placeholder.svg"
+												width="300"
+											/>
+											<div className="grid grid-cols-3 gap-2">
+												<button>
+													<img
+														alt="Product image"
+														className="aspect-square w-full rounded-md object-cover"
+														height="84"
+														src="/placeholder.svg"
+														width="84"
+													/>
+												</button>
+												<button>
+													<img
+														alt="Product image"
+														className="aspect-square w-full rounded-md object-cover"
+														height="84"
+														src="/placeholder.svg"
+														width="84"
+													/>
+												</button>
+												<button className="flex aspect-square w-full items-center justify-center rounded-md border border-dashed">
+													<Upload className="h-4 w-4 text-muted-foreground" />
+													<span className="sr-only">Upload</span>
+												</button>
+											</div>
+										</div>
+									</CardContent>
+								</Card>
+								<Card x-chunk="dashboard-07-chunk-5">
+									<CardHeader>
+										<CardTitle>Archive Product</CardTitle>
+										<CardDescription>
+											Lipsum dolor sit amet, consectetur adipiscing elit.
+										</CardDescription>
+									</CardHeader>
+									<CardContent>
+										<div></div>
+										<Button size="sm" variant="secondary">
+											Archive Product
+										</Button>
+									</CardContent>
+								</Card>
+							</div>
+						</div>
+						<div className="flex items-center justify-center gap-2 md:hidden">
+							<Button variant="outline" size="sm">
+								Discard
+							</Button>
+							<Button size="sm">Save Product</Button>
+						</div>
+					</div>
+				</form>
+			</Form>
+  )
+}
+
+export default CreateUpdateProductForm
