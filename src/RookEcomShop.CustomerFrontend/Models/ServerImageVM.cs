46c832b31da81abd7d90fd84a97e1e9cad885a8d
Ken Nguyen <ngkien299@gmail.com>
2024-05-26 17:04:22 +0700

    feat: Add ServerImageVM model and partial view for server-side images

---
+++ src/RookEcomShop.CustomerFrontend/Models/ServerImageVM.cs
@@ -0,0 +1,9 @@
+namespace RookEcomShop.CustomerFrontend.Models
+{
+    public class ServerImageVM
+    {
+        public string Url { get; set; } = null!;
+        public string? Alt { get; set; }
+        public string? ClassName { get; set; }
+    }
+}
